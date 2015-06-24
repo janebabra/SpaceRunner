f2fe8a6d9f7e536f8090b06fbff03421e284a7e1
Arturo Nereu <arturo.nereu@gmail.com>
2015-06-24 13:04:40 -0500

    Added SpineRunner Unity Project and assets
    Added SpineRunner Unity Project and assets. To be used on it’s won
    repository.

---
+++ Assets/Scripts/CaptainController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaptainController : MonoBehaviour 
+{
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () 
+	{
+		animator = transform.FindChild ("CaptainSprite").GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.A))
+			animator.SetTrigger ("Jump");
+		else if (Input.GetKeyDown (KeyCode.S))
+			animator.SetTrigger ("Slide");
+	}
+}
